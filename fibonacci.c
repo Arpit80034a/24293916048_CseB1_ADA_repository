50b5f1ce95fb162e056a6d093adae1e9a52e531f
Arpit80034a <arpitsuman17@gmail.com>
2025-10-28 14:34:52 +0530

    Add files via upload

--- fibonacci.c
+++ fibonacci.c
@@ -1,34 +1,42 @@
-#include<stdio.h> 
-#include<time.h> 
-#include<stdlib.h> 
-int iter(int n){ 
-    if(n==0){ 
-        return 0;  
-    } 
-    else if(n==1){ 
-        return 1; 
-    } 
-    int a=0; 
-    int b=1; 
-    int ans=0; 
-    for(int i=2;i<=n;i++){ 
-        ans=a+b; 
-        a=b; 
-        b=ans; 
-    } 
-    return ans; 
+#include <stdio.h> 
+#include <windows.h>  // For QueryPerformanceCounter & Frequency 
+ 
+long long bottom_up(int n,long long arr[]){ 
+    arr[0]=0; 
+    arr[1]=1; 
+     
+        for(int i=2;i<=n;i++){ 
+            arr[i]=arr[i-1]+arr[i-2]; 
+        } 
+    return arr[n];  
      
 } 
-int main(){ 
-    for(int i=20;i<=55;i+=5){ 
-        int x=0; 
-        clock_t start=clock(); 
-        for(int j=0;j<1000;j++){ 
-            x=iter(i); 
+ 
+int main() { 
+    LARGE_INTEGER freq, start, end; 
+    QueryPerformanceFrequency(&freq); // ticks per second 
+ 
+    for (int i = 25; i <= 60; i += 5) { 
+        long long arr[i + 1]; 
+        long long x = 0; 
+ 
+        QueryPerformanceCounter(&start); 
+ 
+        for (int j = 0; j < 1000; j++) {   // run 1000 times for averaging 
+            for (int k = 0; k <= i; k++) arr[k] = -1; 
+            x = bottom_up(i, arr); 
         } 
-        clock_t end=clock(); 
-        double t=(double)(end-start)/CLOCKS_PER_SEC/1000; 
-        printf("fibonacci no %d is equal to %d\ntime taken=%e sec\n\n",i,x,t); 
+ 
+        QueryPerformanceCounter(&end); 
+ 
+        long long elapsed_ticks = end.QuadPart - start.QuadPart; 
+        long long elapsed_ns = (elapsed_ticks * 1000000000LL) / freq.QuadPart; 
+        double avg_ns = (double)elapsed_ns / 1000.0; 
+ 
+        printf("fibonacci no %d is equal to %lld\n", i, x); 
+        printf("average time per run = %.5f ns\n", avg_ns); 
+        printf("average time per run = %e s\n\n", avg_ns/1e9); 
  
     } 
-} 
+    return 0; 
+}
